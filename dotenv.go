e1e01c6305378f092e38494fd70327cb80f1ffa2
varunIIT <varutalk@gmail.com>
2025-08-13 22:11:35 +0530

    move common Overload and OverloadWatchFiles logic to overloadFromFiles helper

--- dotenv.go
+++ dotenv.go
@@ -80,11 +80,10 @@ func loadFile(file string, overload bool) error {
 	return nil
 }
 
-func (d *dotenv) Overload() error {
-	parsedFiles := d.opts.ParseFilePaths()
+func (d *dotenv) overloadFromFiles(parsedFiles []string) error {
 	for _, parsedFile := range parsedFiles {
 		if err := loadFile(parsedFile, true); err != nil && d.opts.debug {
-			log.Println(fmt.Sprintf("[dotenv] Overloading parsedFile %s failed with error %s", parsedFile, err.Error()))
+			log.Printf("[dotenv] Overloading parsedFile %s failed with error %s", parsedFile, err.Error())
 			continue
 		}
 		d.files = append(d.files, parsedFile)
@@ -92,16 +91,12 @@ func (d *dotenv) Overload() error {
 	return nil
 }
 
+func (d *dotenv) Overload() error {
+	return d.overloadFromFiles(d.opts.ParseFilePaths())
+}
+
 func (d *dotenv) OverloadWatchFiles() error {
-	parsedFiles := d.opts.ParseWatchFilePaths()
-	for _, parsedFile := range parsedFiles {
-		if err := loadFile(parsedFile, true); err != nil && d.opts.debug {
-			log.Println(fmt.Sprintf("[dotenv] Overloading parsedFile %s failed with error %s", parsedFile, err.Error()))
-			continue
-		}
-		d.files = append(d.files, parsedFile)
-	}
-	return nil
+	return d.overloadFromFiles(d.opts.ParseWatchFilePaths())
 }
 
 func new() *dotenv {
